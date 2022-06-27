34a24efa3594e458536b258f2a7f73a24b4c680e
SolarisPolaris <justinzab4226@gmail.com>
2022-06-26 22:30:00 -0400

    Integration of Current Game System With the UI

--- rp.js
+++ rp.js
@@ -1,38 +1,5 @@
 
 
-
-
-
-function playerSelection(){
-
-    let playerInput;
-
-    //continue loop until player inputs the correct values
-    while (playerInput != "rock" && playerInput != "paper" && playerInput != "scissors"){
-
-        playerInput = prompt("Rock, Paper, Scissors. Please Chose One: ");
-        if (playerInput == null) continue;
-        playerInput = playerInput.toLowerCase();
-
-        //shorthand commands to simplify input
-        switch (playerInput){
-            case "r":
-                playerInput = "rock";
-                break;
-            case "p":
-                playerInput = "paper";
-                break;
-            case "s":
-                playerInput = "scissors";
-        }
-    }
-
-    return playerInput;
-    
-
-
-}
-
 function computerSelection(){
 
     //get a random number from 0 to 99
@@ -110,44 +77,13 @@ function getResults(playerI, computerI){
     return result;
 }
 
-function playGame(){
-    let maxRound = 0;
-    let results = [];
-    let winsComputer = 0;
-    let winsPlayer = 0;
-    let winner = "";
-    
+function playGame(playerInput){
 
-    //continues input prompt until input is between 0 and 101
-    while (maxRound <= 0 || maxRound > 100 || !Number.isInteger(parseInt(maxRound))){
-        maxRound = prompt("Input Number of Rounds to be Played (Max is 100)");
-    }
-
-    //loop for the number of rounds input
-    for(let i = 0; i < maxRound; i++){
-        console.log("Round " + (i+1));
-        results = getResults(playerSelection(), computerSelection());
-
-        //increase the winner count of the player/computer side
-        if(results[1] === "player") winsPlayer++;
-        else winsComputer++;
-
-        console.log(results[0]);
-    }
-
-    //display the winner of the match
-
-    console.log("Computer: " + winsComputer);
-    console.log("Player: " + winsPlayer);
-    if (winsComputer === winsPlayer) console.log("No One Wins! THe game is tied.");
-    else{
-        if (winsComputer > winsPlayer) winner = "computer. Too Bad!";
-        else winner = "player. Congrats!";
-       
-        console.log("The winner is the " + winner);
-    }
-
+    console.log(getResults(playerInput, computerSelection())[0]);
+   
 
 }
+
+
 
 
